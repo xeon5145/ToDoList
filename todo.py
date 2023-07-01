b26ca6ea99f1f558c55999b49b57db003c1da26b
Abhishek Kagra <abhishekkagra@aaddoo.net>
2023-07-01 14:09:28 +0530

    show feature from file added

--- todo.py
+++ todo.py
@@ -17,10 +17,13 @@ while True:
             file = open('todo.txt', 'w')
             file.writelines(todo_list)
         case 'show':
+            file = open('todo.txt','r')
+            todo_list = file.readlines()
+            file.close()
+
             # getting list of todos
             for index,item in enumerate(todo_list):
                 item = item.title()
-
                 # making item coutn to start form 1 instead of 0
                 index = index+1
                 data = f"{index}. {item}"
